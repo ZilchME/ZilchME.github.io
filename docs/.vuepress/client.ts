4c8efba79ffd49496baa1ddb4242b1ead3eebec1
ZilchME <1355478132@qq.com>
2026-04-30 21:36:25 +0800

    add plugin-llms

--- docs/.vuepress/client.ts
+++ docs/.vuepress/client.ts
@@ -1,8 +1,11 @@
+import { defineAsyncComponent, h } from 'vue'
 import { defineClientConfig } from 'vuepress/client'
 // import RepoCard from 'vuepress-theme-plume/features/RepoCard.vue'
 // import NpmBadge from 'vuepress-theme-plume/features/NpmBadge.vue'
 // import NpmBadgeGroup from 'vuepress-theme-plume/features/NpmBadgeGroup.vue'
 // import Swiper from 'vuepress-theme-plume/features/Swiper.vue'
+import { Layout } from 'vuepress-theme-plume/client'
+import PageContextMenu from 'vuepress-theme-plume/features/PageContextMenu.vue'
 
 import CustomComponent from './theme/components/Custom.vue'
 
@@ -18,5 +21,11 @@ export default defineClientConfig({
 
     // your custom components
     app.component('CustomComponent', CustomComponent)
+  },
+  layouts: {
+    Layout: h(Layout, null, {
+      // 将 PageContextMenu 添加到 doc-title-after 插槽，即文章标题的右侧
+      'doc-title-after': () => h(PageContextMenu), 
+    }),
   },
 })
