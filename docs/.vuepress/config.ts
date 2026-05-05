731b5fd153aaf61f8dbf9e892dc51aad268d5237
ZilchME <1355478132@qq.com>
2026-05-05 22:16:03 +0800

    feat: 启用 annotation 语法并修改文档分类文档

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -94,7 +94,7 @@ export default defineUserConfig({
      */
     markdown: {
       abbr: true,         // 启用 abbr 语法  *[label]: content
-      //   annotation: true,   // 启用 annotation 语法  [+label]: content
+        annotation: true,   // 启用 annotation 语法  [+label]: content
       //   pdf: true,          // 启用 PDF 嵌入 @[pdf](/xxx.pdf)
       //   caniuse: true,      // 启用 caniuse 语法  @[caniuse](feature_name)
       //   plot: true,         // 启用隐秘文本语法 !!xxxx!!
